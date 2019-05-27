2c60c97773fda7194210e43cfbcc677994a15230
ksaketh10 <ksaketh10@gmail.com>
2019-05-27 11:09:04 +0530

    Updates

---
+++ precache-manifest.036c9d11132b9880f5741f66a579e7ae.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0276b140662e1b6559ec0d4c512ad9f7",
+    "url": "/ztalent-react/index.html"
+  },
+  {
+    "revision": "c08a27d9692904d17195",
+    "url": "/ztalent-react/static/css/main.85911dc8.chunk.css"
+  },
+  {
+    "revision": "e204f532e150985a2fb4",
+    "url": "/ztalent-react/static/js/0.bf73845f.chunk.js"
+  },
+  {
+    "revision": "dd99ffde74403662f7cd",
+    "url": "/ztalent-react/static/js/3.0d77044d.chunk.js"
+  },
+  {
+    "revision": "e55dc363fd249fc00648",
+    "url": "/ztalent-react/static/js/4.25cf4a61.chunk.js"
+  },
+  {
+    "revision": "629371f8aae19dc79a28",
+    "url": "/ztalent-react/static/js/5.c6761c98.chunk.js"
+  },
+  {
+    "revision": "83af57a3b337fd5e1f89",
+    "url": "/ztalent-react/static/js/6.cb9a5181.chunk.js"
+  },
+  {
+    "revision": "c08a27d9692904d17195",
+    "url": "/ztalent-react/static/js/main.983cb2e4.chunk.js"
+  },
+  {
+    "revision": "19d7f8ce1ba3fe86df9b",
+    "url": "/ztalent-react/static/js/runtime~main.2d748751.js"
+  },
+  {
+    "revision": "1a280523d375e9358d5229df34fc8e94",
+    "url": "/ztalent-react/static/media/poppins-regular.1a280523.woff2"
+  },
+  {
+    "revision": "46a7d48240d428c9dc3d4ff579199312",
+    "url": "/ztalent-react/static/media/poppins-regular.46a7d482.woff"
+  },
+  {
+    "revision": "528cb4a87fbf5acd1aa772752b6848b1",
+    "url": "/ztalent-react/static/media/poppins-regular.528cb4a8.eot"
+  },
+  {
+    "revision": "6eb275050498ab5692aff58bdd7a2643",
+    "url": "/ztalent-react/static/media/poppins-regular.6eb27505.ttf"
+  },
+  {
+    "revision": "115b9c186536dd9d22d48d3b0967a216",
+    "url": "/ztalent-react/static/media/zemoso-logo.115b9c18.svg"
+  }
+]);
