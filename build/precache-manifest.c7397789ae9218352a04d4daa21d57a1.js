fd50f1c7aee918653af8ab7d55e0b0e6ae849a74
ksaketh10 <ksaketh10@gmail.com>
2019-06-12 17:24:50 +0530

    Add build folder temp

---
+++ build/precache-manifest.c7397789ae9218352a04d4daa21d57a1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46799105728e4eb70d7af10dd78c2ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7a61980d6d7a74238ac",
+    "url": "/static/css/main.14c6cd0c.chunk.css"
+  },
+  {
+    "revision": "e70d44534eb11c671f5f",
+    "url": "/static/js/0.6cf1c9d5.chunk.js"
+  },
+  {
+    "revision": "818a2f6846d268a91ff3",
+    "url": "/static/js/3.6102deab.chunk.js"
+  },
+  {
+    "revision": "73131900d914a0b60eee",
+    "url": "/static/js/4.3a8e081f.chunk.js"
+  },
+  {
+    "revision": "ad9a5c9f4c65f9f5adb4",
+    "url": "/static/js/5.a3a51e00.chunk.js"
+  },
+  {
+    "revision": "61ad92ac7c20e39b3197",
+    "url": "/static/js/6.a7bff481.chunk.js"
+  },
+  {
+    "revision": "d7a61980d6d7a74238ac",
+    "url": "/static/js/main.e02733d4.chunk.js"
+  },
+  {
+    "revision": "8033bc5923ad77dfc5e4",
+    "url": "/static/js/runtime~main.b5a3ca31.js"
+  },
+  {
+    "revision": "1a280523d375e9358d5229df34fc8e94",
+    "url": "/static/media/poppins-regular.1a280523.woff2"
+  },
+  {
+    "revision": "46a7d48240d428c9dc3d4ff579199312",
+    "url": "/static/media/poppins-regular.46a7d482.woff"
+  },
+  {
+    "revision": "528cb4a87fbf5acd1aa772752b6848b1",
+    "url": "/static/media/poppins-regular.528cb4a8.eot"
+  },
+  {
+    "revision": "6eb275050498ab5692aff58bdd7a2643",
+    "url": "/static/media/poppins-regular.6eb27505.ttf"
+  },
+  {
+    "revision": "115b9c186536dd9d22d48d3b0967a216",
+    "url": "/static/media/zemoso-logo.115b9c18.svg"
+  }
+]);
--- src/_constants/UriConstants.js
+++ src/_constants/UriConstants.js
@@ -1,5 +1,5 @@
- //export const API_BASE_URL = 'https://ztalent.gq:8443';
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://ztalent.gq:8443';
+//export const API_BASE_URL = 'http://localhost:8080';
 export const CURRENT_USER = 'currentUser';
 
 // Employee Urls
